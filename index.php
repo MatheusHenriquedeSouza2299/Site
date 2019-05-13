cb721fcb95db2875b979347bf35ecfcb683e2061
MatheusHenriquedeSouza <48722541+Matheusdelicinha@users.noreply.github.com>
2019-05-13 09:54:14 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,41 +7,41 @@
     <div class="col-sm-4">
       <h2>Sobre Mim</h2>
       <h5>Minha Foto:</h5>
-      <div class="fakeimg"><img src="minhafoto.jpg" width="220px" height="200px"></div>
+      <div class="fakeimg"><img src="minhafoto.jpg" width="210px" height="200px"></div>
       <p>aluno mais gostoso do curso técnico.</p>
       <h3>Alguns links</h3>
       <p>Locais de cursos</p>
       <ul class="nav nav-pills flex-column">
         <li class="nav-item">
-          <a class="nav-link" href="#">City Computer</a>
+          <a class="nav-link" href="http://www.citycomputer.com.br/SelecaoDeUnidades">City Computer</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Lisboa</a>
+          <a class="nav-link" href="http://www.swwantoniolisboa.seed.pr.gov.br/modules/noticias/">Lisboa</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">cebrac</a>
+          <a class="nav-link" href="https://cebrac.com.br/">cebrac</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Ciee</a>
+          <a class="nav-link" href="https://portal.ciee.org.br/">Ciee</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Promec</a>
+          <a class="nav-link disabled" href="http://www.ufrgs.br/promec">Promec</a>
         </li>
       </ul>
       <hr class="d-sm-none">
     </div>
     <div class="col-sm-8">
       <h2>Matheus Henrique de Souza</h2>
+      <h5>Data de aniversário</h5>
+      <div class="fakeimg"><img src="qualquer.jpg" alt="" width="400px" height="200px"></div>
       
-      <div class="fakeimg"><img src="data.jpg" alt="" width="400px" height="200px"></div>
-      <p>Data de Aniversário</p>
-      <p>O nascimento de Matheus Henrique de Souza, filho de Maria Rúbia e de José Alves de Souza, nascido em 24/03/2002 (vinte e quatro de março de dois mil e dois)</p>
+      <p>A data do meu aniversário é 24/03/2002</p>
       <br>
       <h2>Usa Drogas?</h2>
-    
-      <div class="fakeimg"><img src="qualquer.jpg" alt="" width="400px" height="200px"></div>
-      <p>Uso drogas?</p>
-      <p>NÃO.</p>
+      <h5>Sera que uso algum tipo de drogas?</h5>
+      <div class="fakeimg"><img src="qualquer2.jpeg" alt="" width="400px" height="200px"></div>
+      <p>Sera que uso drogas?</p>
+      <p>Não. Não uso nem um tipo de drogas, nem alco nem nada.</p>
     </div>
   </div>
 </div>
---
+++ quemsomos.php
@@ -0,0 +1,20 @@
+<?php
+	include("cabecalho.php");
+?>
+
+<center><div class="col-sm-8">
+      <h2>Quem sou eu?</h2>
+      
+      <div class="fakeimg"><img src="int.png" alt="" width="170px" height="170px"></div>
+      
+      <p>?????????????????????</p>
+      <br>
+      
+      <p>Aluno do Colégio ântonio Francisco lisboa, cursando 3º ano do ensino técnico em informatica, Atualmente trabalhando no correto de seguros A7.
+      17 anos de idade.</p>
+      
+  </div></center>
+</div></center>
+<?php
+	include("rodape.php");	
+?>
